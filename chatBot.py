d92b1d19a1d46e82a8378af0578d26911429a9f7
Anup Jeejo <jeejoanup@gmail.com>
2025-10-19 18:31:07 +0530

    removed unwanted imports and added chatbot agent logic

--- chatBot.py
+++ chatBot.py
@@ -1,7 +1,6 @@
-import os
 from dotenv import load_dotenv
-import strealit as st
-from lanchain_groq import ChatGroq
+import streamlit as st
+from langchain_groq import ChatGroq
 
 #load the env variables
 load_dotenv()
@@ -14,3 +13,58 @@ st.set_page_config(
 )
 
 st.title("💬 Generative AI chatBot")
+
+#initiate chat history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+    
+#show chat history
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+        
+#llm initiate
+llm = ChatGroq(
+    model = "openai/gpt-oss-120b",
+    temperature = 0.0
+)
+
+#input box
+#user enters input here
+user_prompt = st.chat_input("Ask chatbot...")
+
+if user_prompt:
+    #display user prompt on UI
+    st.chat_message("user").markdown(user_prompt)
+    
+    #add the user promt to chat history
+    st.session_state.chat_history.append(
+        {
+            "role": "user", 
+            "content": user_prompt
+        }
+    )
+    
+    #call the llm with and pass the chat history along with it
+    response = llm.invoke(
+        input = [
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant",
+                },
+                *st.session_state.chat_history
+            ]
+    )
+    
+    #store llm response to chat history
+    assistant_response = response.content
+    st.session_state.chat_history.append(
+        {
+            "role": "assistant",
+            "content": assistant_response,
+        }
+    )
+    
+    #display llm response
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
